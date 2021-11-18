5133165ddcfd1f3098412976be39e8a36650d2bd
nhatpd <nhatpd@vmodev.com>
2021-11-18 14:09:11 +0700

    update footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <p>This is footer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
